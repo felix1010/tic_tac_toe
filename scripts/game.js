2874430cb5565b73c9ae344f44038221f8e3a96e
felix1010 <xu_yinlong@live.cn>
2023-03-13 21:48:00 +0100

    check winner

--- scripts/game.js
+++ scripts/game.js
@@ -20,7 +20,63 @@ function selectGameField(event) {
   if (event.target.tagName !== "LI") {
     return;
   }
-  event.target.textContent = players[activePlayer].symbol;
-  event.target.classList.add("disabled");
+
+  const selectedField = event.target;
+  const selectdColumn = selectedField.dataset.col - 1;
+  const selectedRow = selectedField.dataset.row - 1;
+
+  if (gameData[selectedRow][selectdColumn] > 0) {
+    alert("Select an empty field!");
+    return;
+  }
+
+  selectedField.textContent = players[activePlayer].symbol;
+  selectedField.classList.add("disabled");
+
+  gameData[selectedRow][selectdColumn] = activePlayer + 1;
+
+  const winnerId = checkForGameOver();
+  console.log(winnerId);
+
   switchPlayer();
+}
+
+function checkForGameOver() {
+  for (let i = 0; i <= 2; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+
+  for (let i = 0; i <= 2; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[1][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+
+  if (
+    gameData[0][2] > 0 &&
+    gameData[0][2] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][0]
+  ) {
+    return gameData[0][2];
+  }
+
+  return 0;
 }
